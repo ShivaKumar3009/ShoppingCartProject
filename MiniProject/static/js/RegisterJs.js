81435c30992288ac2ffa3eecdd76cd54eb917195
Shiva Kumar <kurvashiva.99@gmail.com>
2020-06-30 16:42:27 +0530

    "seventh commit"

--- MiniProject/static/js/RegisterJs.js
+++ MiniProject/static/js/RegisterJs.js
@@ -4,18 +4,16 @@ function validate() {
   var e = document.getElementById("email").value;
   var p1 = document.getElementById("pass1").value;
   var p2 = document.getElementById("pass2").value;
-  var m = document.getElementById("mob").value;
+  //var m = document.getElementById("mob").value;
   var ereg = /^([\.-\w]+)@([\.-\w]+).([a-z]{2,10})$/;
-  var mreg = /^[6-9]\d{9}$/;
+  //var mreg = /^[6-9]\d{9}$/;
   if (u.trim() == "" || p1.trim() == "") {
     alert("enter valid Username or Password");
     return false;
-  }
-  if (!ereg.test(e)) {
+  } else if (!ereg.test(e)) {
     alert("Enter valid email id");
     return false;
-  }
-  if (p1 != p2) {
+  } else if (p1 != p2) {
     alert("Password not matching");
     return false;
   } else {
